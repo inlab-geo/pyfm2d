5eb8cc336d9f6392b3a1407fc279961f5ab30113
Juerg Hauser <juerg.hauser@gmail.com>
2024-12-18 09:30:30 +1100

    feat: read, set and get receivers from python

--- src/pyfm2dss/_pyfm2dss.py
+++ src/pyfm2dss/_pyfm2dss.py
@@ -29,7 +29,7 @@ class FastMarchingMethod:
             scz_.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
             ctypes.byref(nsrc_),
         )
-
+        
     def get_sources(self):
         nsrc_ = ctypes.c_int(-99)
         self.libfm2dss.get_number_of_sources(ctypes.byref(nsrc_))
@@ -43,4 +43,31 @@ class FastMarchingMethod:
         )
         scx = numpy.array(scx_)
         scz = numpy.array(scz_)
+        return scx, scz
+
+    def read_receivers(self, fn_):
+        fn = ctypes.c_char_p(fn_.encode("UTF-8"))
+        self.libfm2dss.read_receivers(fn, ctypes.c_int(len(fn.value)))
+
+    def set_receivers(self, rcx_, srz_):
+        nrc_ = ctypes.c_int(len(rcx_))
+        self.libfm2dss.set_receivers(
+            rcx_.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+            rcz_.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+            ctypes.byref(nrc_),
+        )
+        
+    def get_receivers(self):
+        nsrc_ = ctypes.c_int(-99)
+        self.libfm2dss.get_number_of_sources(ctypes.byref(nrc_))
+        nrc = nrc_.value
+        rcx_ = numpy.empty((nrc), dtype=ctypes.c_float)
+        rcz_ = numpy.empty((nrc), dtype=ctypes.c_float)
+        self.libfm2dss.get_sources(
+            rcx_.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+            rcz_.ctypes.data_as(ctypes.POINTER(ctypes.c_float)),
+            ctypes.byref(nrrc_),
+        )
+        rcx = numpy.array(rcx_)
+        rcz = numpy.array(rcz_)
         return scx, scz
--- test/test_run.py
+++ test/test_run.py
@@ -11,4 +11,9 @@ fmm = pyfm2dss.FastMarchingMethod()
 fmm.read_sources("sources.dat")
 scx, scz = fmm.get_sources()
 fmm.set_sources(scx, scz)
+
+fmm.read_receivers("receivers.dat")
+#rcx, rcz = fmm.get_sources()
+#fmm.set_sources(rcx, rcz)
+
 fmm.run()
