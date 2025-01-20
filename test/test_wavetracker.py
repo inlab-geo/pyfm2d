7348098d9537d3065c51a8fdcc5e75e5fc655c10
auggiemarignier <augustin.marignier@anu.edu.au>
2025-01-20 14:25:14 +1100

    test: more rigorous testing of traveltimes

--- test/test_wavetracker.py
+++ test/test_wavetracker.py
@@ -11,32 +11,35 @@ from pyfm2d import WaveTrackerOptions, display_model, BasisModel
 from pyfm2d.wavetracker import _calc_wavefronts_process, _calc_wavefronts_multithreading
 
 PLOT = True
+HOMOGENOUS_VELOCITY = 2.0
 
 
 def get_sources():
-    return np.array([[0.1, 0.15]])
+    # to ensure separation of sources and receivers
+    # get some random sources in one quadrant
+    return np.random.uniform(0.05, 0.45, (5, 2))
 
 
 def get_receivers():
-    return np.array([[0.8, 1], [1.0, 0.6]])
+    # to ensure separation of sources and receivers
+    # get some random receivers in the opposite quadrant
+    return np.random.uniform(0.55, 0.95, (3, 2))
 
 
 def create_velocity_grid_model():
-    m = np.array(
-        [
-            [1, 1.1, 1.1, 1.0],
-            [1.0, 1.2, 1.4, 1.3],
-            [1.1, 1.2, 1.3, 1.2],
-            [1.1, 1.1, 1.2, 1.2],
-        ]
-    )
+    # Create a simple velocity model that we can easily
+    # manually calculate the travel times for.
+    # In this case, we have a 5x5 grid with a velocity of 2.0
+    # so the travel time from the source to the receiver should be
+    # 0.5 times the path length.
+    m = np.ones((5, 5)) * HOMOGENOUS_VELOCITY
     g = BasisModel(m)
-    mp = g.get_velocity()
-    mp[1, 1] = 0.7
-    mp[2, 2] = 0.9
-    mp[2, 1] = 1.3
-    g.set_velocity(mp)
     return g
+
+
+def calculate_expected_tt(src, rec):
+    diff = (src[:, np.newaxis] - rec).reshape(-1, 2)  # some broadcasting magic
+    return np.sqrt(np.sum(diff ** 2, axis=1)) / HOMOGENOUS_VELOCITY
 
 
 def test__calc_wavefonts_process():
@@ -55,6 +58,13 @@ def test__calc_wavefonts_process():
     assert result.ttimes is not None
     assert result.paths is not None
     assert result.frechet is not None
+
+    # Check the travel times
+    expected_tt = calculate_expected_tt(srcs, recs)
+
+    # fmm seems quite inaccurate because of the small grid size
+    # and putting float32 everywhere
+    assert np.allclose(result.ttimes, expected_tt, atol=1e-2)
 
     if PLOT:
         display_model(g.get_velocity(), paths=result.paths)
@@ -77,3 +87,7 @@ def test_calc_wavefonts_multithreading():
     assert result.ttimes is not None
     assert result.paths is not None
     assert result.frechet is not None
+
+    # Check the travel times
+    expected_tt = calculate_expected_tt(srcs, recs)
+    assert np.allclose(result.ttimes, expected_tt, atol=1e-2)
