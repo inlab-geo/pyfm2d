8fccf3977c5dac4a0788e12ef827dc740645b361
auggiemarignier <augustin.marignier@anu.edu.au>
2025-03-13 06:05:20 +0000

    test: setup required to allocate

--- test/test_wavetracker.py
+++ test/test_wavetracker.py
@@ -1,19 +1,13 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jan 10 08:33:05 2025
-
-@author: malcolm
-"""
 import numpy as np
-
+import pytest
 from pyfm2d import WaveTrackerOptions, display_model, BasisModel
 from pyfm2d.wavetracker import (
     _calc_wavefronts_process,
     _calc_wavefronts_multithreading,
     cleanup,
-    _build_velocity_grid
+    _build_velocity_grid,
 )
+import pyfm2d.fastmarching as fmm
 
 PLOT = False
 HOMOGENOUS_VELOCITY = 2.0
@@ -79,9 +73,54 @@ def test_cleanup():
 
     @cleanup
     def failing_function():
+        # setup required to allocate arrays in the first place
+        v = create_velocity_grid_model().get_velocity()
+        recs = get_receivers()
+        srcs = get_sources()
+        options = WaveTrackerOptions(times=True, paths=True, frechet=True)
+        fmm.set_solver_options(
+            options.dicex,
+            options.dicey,
+            options.sourcegridrefine,
+            options.sourcedicelevel,
+            options.sourcegridsize,
+            options.earthradius,
+            options.schemeorder,
+            options.nbsize,
+            options.lttimes,
+            options.lfrechet,
+            options.tsource,
+            options.lpaths,
+        )
+
+        fmm.set_sources(srcs[:, 1], srcs[:, 0])  # ordering inherited from fm2dss.f90
+        fmm.set_receivers(recs[:, 1], recs[:, 0])  # ordering inherited from fm2dss.f90
+
+        nvx, nvy = v.shape
+        # grid node spacing in lat and long
+        extent = [0, 1, 0, 1]
+        dlat = (extent[3] - extent[2]) / (nvy - 1)
+        dlong = (extent[1] - extent[0]) / (nvx - 1)
+
+        vc = _build_velocity_grid(v)
+
+        fmm.set_velocity_model(nvy, nvx, extent[3], extent[0], dlat, dlong, vc)
+
+        # set up time calculation between all sources and receivers
+        associations = np.ones((recs.shape[0], srcs.shape[0]))
+        fmm.set_source_receiver_associations(associations)
+
+        fmm.allocate_result_arrays()
         raise ValueError("This function should fail")
 
-    failing_function()
+    with pytest.raises(ValueError):
+        failing_function()
+
+    # If the clean up worked, we should now be able to
+    # allocate again without any issues
+    fmm.allocate_result_arrays()
+    # Now cleanup again to avoid any issues with the next test
+    fmm.deallocate_result_arrays()
 
 
 def test_calc_wavefonts_multithreading():
