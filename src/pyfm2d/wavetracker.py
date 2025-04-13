fixed problem to Frechet derivative lower bound.
Frechet derivatives of travel time with respect to grid velocities had a fixed floor value of 1.0E-6. For certain problems, e.g. in Cartesian co-ordinates where velocities are in m/s and have values of 10^3, this can cause derivatives of travel time with respect to slowness to have a floor of ~1.0, since dt/ds = -(v**2)*dt/dv. Hence slowness derivative matrix can be set to zero which causes flow on errors in tomography examples. Have replaced the floor with 1.0E-10 for now. This may require further work.

src/pyfm2d/wavetracker.py
@@ -364,8 +364,7 @@ def _get_frechet_derivatives(cartesian, velocityderiv, velocity):
     # reformat as a sparse CSR matrix
     frechetvals = csr_matrix(F.reshape((nrays, nx * ny)))
 
-    if not velocityderiv:
-        # adjust derivatives to be of velocites rather than slownesses (default)
+    if not velocityderiv: # change derivatives to slowness (default), otherwise stay as velocity.
         x2 = -(velocity * velocity).reshape(-1)
         frechetvals = frechetvals.multiply(x2)
 